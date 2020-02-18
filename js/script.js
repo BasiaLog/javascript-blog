3ecd9e2e81da774ba837e21a3da32a42343fe9c3
Basia Pisarek <barbara.pisarek12@gmail.com>
2020-02-18 01:05:01 +0100

    Create new titleLinks

--- js/script.js
+++ js/script.js
@@ -62,25 +62,30 @@ function generateTitleLinks(){
   
 
   /* for each article */
+   const articles = document.querySelectorAll(optArticleSelector);
 
+   for (let article of articles) {
+
+   
     /* get the article id */
-
-
+   const articleId = article.getAttribute('id');
+   console.log('articleId');
 
     /* find the title element */
-
-
-
-    /* get the title from the title element */
-
-
+   const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+   console.log('articleTitle');
+    
+    
     /* create HTML of the link */
-
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('linkHTML');
 
 
     /* insert link into titleLinks */
+    titleList.innerHTML = titleList.innerHTML + linkHTML;
+   }
 
-
+   
 }
 
 generateTitleLinks();
